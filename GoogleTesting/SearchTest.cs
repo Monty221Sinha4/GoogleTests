15c626ba6a0962f68776cb16a157a63f7a46c719
Monty Sinha <monty.sinha2013@live.co.uk>
2020-08-04 16:17:31 +0100

    Add files via upload

---
+++ GoogleTesting/SearchTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace GoogleTesting
+{
+    [TestClass]
+    public class SearchTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("Start Maximed");
+            IWebDriver webDriver = new ChromeDriver(options);
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(30));
+            webDriver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement textField = webDriver.FindElement(By.Name("q"));
+            textField.SendKeys("Automation testing");
+            IWebElement searchButton = webDriver.FindElement(By.CssSelector("Input[value='Google Search']"));
+            searchButton.Click();
+            webDriver.Dispose();
+        }
+
+
+    }
+}
